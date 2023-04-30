ee87b0c01cb5074109c622a1c52d98d6109b69d1
manoranjith-shankar <21csbs023@skcet.ac.in>
2023-04-30 21:46:42 +0530

    initFrontend

---
+++ src/Header/Header.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header id="header">
+            {/* Navbar */}
+            <nav data-aos="zoom-out" data-aos-delay={800} className="navbar navbar-expand">
+                <div className="container header">
+                    {/* Navbar Brand*/}
+                    <a className="navbar-brand" href="/">
+                        <img className="navbar-brand-sticky" src="img/logo.png" alt="sticky brand-logo" />
+                    </a>
+                    <div className="ml-auto" />
+                    {/* Navbar */}
+                    <ul className="navbar-nav items mx-auto">
+                        <li className="nav-item dropdown">
+                            <a className="nav-link" href="/">Home</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link" href="#">Explore <i className="fas fa-angle-down ml-1" /></a>
+                            <ul className="dropdown-menu">
+                                <li className="nav-item"><a href="/explore-1" className="nav-link">Open Raffles</a></li>
+                                <li className="nav-item"><a href="/explore-1" className="nav-link">Unlisted Raffles</a></li>
+                            </ul>
+                        </li>
+                        <li className="nav-item">
+                            <a href="/activity" className="nav-link">Activity</a>
+                        </li>
+                    </ul>
+                    {/* Navbar Icons */}
+                    <ul className="navbar-nav icons">
+                        <li className="nav-item">
+                            <a href="#" className="nav-link" data-toggle="modal" data-target="#search">
+                                <i className="fas fa-search" />
+                            </a>
+                        </li>
+                    </ul>
+                    {/* Navbar Toggler */}
+                    <ul className="navbar-nav toggle">
+                        <li className="nav-item">
+                            <a href="#" className="nav-link" data-toggle="modal" data-target="#menu">
+                                <i className="fas fa-bars toggle-icon m-0" />
+                            </a>
+                        </li>
+                    </ul>
+                    {/* Navbar Action Button */}
+                    <ul className="navbar-nav action">
+                        <li className="nav-item ml-3">
+                            <a href="/wallet-connect" className="btn ml-lg-auto btn-bordered-white"><i className="icon-wallet mr-md-2" />Wallet Connect</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;

ee51b8d039b392cf388fd05ca379a2a13ee2d61f
manoranjith-shankar <21csbs023@skcet.ac.in>
2023-05-01 12:59:59 +0530

    updateApp

--- src/App.js
+++ src/App.js
@@ -9,3 +9,29 @@ function App() {
 }
 
 export default App;
+
+
+//components:
+/* 
+1. Header
+2. Breadcrumb
+3. Login section
+4. signup section
+5. Hero
+6. creates
+7. expand
+8. explore
+9. activity
+10. connect wallet
+11. Item list
+12. Item Detail
+13. Modal Search
+14. sidebar (*not required)[370x660]
+----------------------------------------------------------------
+----------------------------------------------------------------
+Backend
+15. contract development
+16. buildbear mainnet forking
+17. contract testing
+
+*/

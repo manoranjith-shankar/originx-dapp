cdd198eee85edaebfaca724beca758110462f47b
manoranjith-shankar <21csbs023@skcet.ac.in>
2023-05-06 14:44:34 +0530

    contractUpdate

--- backend/contracts/originx.sol
+++ backend/contracts/originx.sol
@@ -9,24 +9,24 @@ contract nftRafflerOriginx is IERC721Receiver {
     using SafeMath for uint256;
 
     struct Raffle {
-    string raffleName;
-    uint256 nftPrice;
-    uint256 totalVolumeofTickets;
-    uint256 endTime;
-    uint256 nftId;
-    address nftContractAddress;
-    string nftSourceLink;
-    address raffleCreator;
-    uint256 rafflePool;
-    uint256 ticketPrice;
-    uint256 totalSoldTickets;
-    bool raffleCancelled;
-    bool raffleEnded;
-    uint256[] tickets;
+        string raffleName;
+        uint256 nftPrice;
+        uint256 totalVolumeofTickets;
+        uint256 endTime;
+        uint256 nftId;
+        address nftContractAddress;
+        string nftSourceLink;
+        address raffleCreator;
+        uint256 rafflePool;
+        uint256 ticketPrice;
+        uint256 totalSoldTickets;
+        bool raffleCancelled;
+        bool raffleEnded;
+        uint256[] tickets;
     }
 
     struct TicketOwnership {
-       mapping(address => uint256) ticketsOwned;
+        mapping(address => uint256) ticketsOwned;
     }
 
     mapping(uint256 => Raffle) private raffles;
@@ -43,56 +43,79 @@ contract nftRafflerOriginx is IERC721Receiver {
     // }
 
     function generateRaffleID() public view returns (uint256[4] memory) {
-    uint256[4] memory raffleID; 
-    raffleID[0] = uint256(keccak256(abi.encodePacked(
-        block.timestamp, 
-        block.difficulty, 
-        msg.sender, 
-        block.coinbase
-    )));
-    return raffleID;
+        uint256[4] memory raffleID;
+        raffleID[0] = uint256(
+            keccak256(
+                abi.encodePacked(
+                    block.timestamp,
+                    block.difficulty,
+                    msg.sender,
+                    block.coinbase
+                )
+            )
+        );
+        return raffleID;
     }
 
-    function createRaffle(string memory _raffleName, uint256 _nftPrice, uint256 _totalVolumeofTickets, uint256 _endTime, uint256 _nftId, address _nftContractAddress, string memory _nftSourceLink) public returns (uint256[4] memory) {
-    require(_nftContractAddress != address(0), "Invalid NFT contract address");
-    require(_endTime > block.timestamp, "End time must be in future");
-    IERC721 nftContract = IERC721(_nftContractAddress);
-    require(msg.sender == nftContract.ownerOf(_nftId), "Only NFT owner can create raffle");
+    function createRaffle(
+        string memory _raffleName,
+        uint256 _nftPrice,
+        uint256 _totalVolumeofTickets,
+        uint256 _endTime,
+        uint256 _nftId,
+        address _nftContractAddress,
+        string memory _nftSourceLink
+    ) public returns (uint256[4] memory) {
+        require(
+            _nftContractAddress != address(0),
+            "Invalid NFT contract address"
+        );
+        require(_endTime > block.timestamp, "End time must be in future");
+        IERC721 nftContract = IERC721(_nftContractAddress);
+        require(
+            msg.sender == nftContract.ownerOf(_nftId),
+            "Only NFT owner can create raffle"
+        );
 
-    nftContract.safeTransferFrom(msg.sender, address(this), _nftId);
+        nftContract.safeTransferFrom(msg.sender, address(this), _nftId);
 
-    raffleCount++;
-    uint256[4] memory raffleID = generateRaffleID();
+        raffleCount++;
+        uint256[4] memory raffleID = generateRaffleID();
 
-    uint256 raffleIndex = raffleID[0];
+        uint256 raffleIndex = raffleID[0];
 
-    raffles[raffleIndex] = Raffle({
-        raffleName: _raffleName,
-        nftPrice: _nftPrice,
-        totalVolumeofTickets: _totalVolumeofTickets,
-        endTime: _endTime,
-        nftId: _nftId,
-        nftContractAddress: _nftContractAddress,
-        nftSourceLink: _nftSourceLink,
-        raffleCreator: msg.sender,
-        rafflePool: (_nftPrice.mul(150)).div(100),
-        ticketPrice: 0,
-        totalSoldTickets: 0,
-        raffleCancelled: false,
-        raffleEnded: false,
-        tickets: new uint256[](0)
-    });
+        raffles[raffleIndex] = Raffle({
+            raffleName: _raffleName,
+            nftPrice: _nftPrice,
+            totalVolumeofTickets: _totalVolumeofTickets,
+            endTime: _endTime,
+            nftId: _nftId,
+            nftContractAddress: _nftContractAddress,
+            nftSourceLink: _nftSourceLink,
+            raffleCreator: msg.sender,
+            rafflePool: (_nftPrice.mul(150)).div(100),
+            ticketPrice: 0,
+            totalSoldTickets: 0,
+            raffleCancelled: false,
+            raffleEnded: false,
+            tickets: new uint256[](0)
+        });
 
-    raffles[raffleIndex].ticketPrice = raffles[raffleIndex].rafflePool.div(_totalVolumeofTickets);
+        raffles[raffleIndex].ticketPrice = raffles[raffleIndex].rafflePool.div(
+            _totalVolumeofTickets
+        );
 
-    raffleIDToRandomNumber[raffleIndex] = raffleID;
+        raffleIDToRandomNumber[raffleIndex] = raffleID;
 
-    return raffleID;
+        return raffleID;
     }
 
     function setTicketPrice(uint256 _raffleID) public {
         Raffle storage raffle = raffles[_raffleID];
-        require(raffle.raffleCreator == msg.sender, "Only raffle creator can set ticket price");
+        require(
+            raffle.raffleCreator == msg.sender,
+            "Only raffle creator can set ticket price"
+        );
         require(raffle.ticketPrice == 0, "Ticket price already set");
 
         raffle.ticketPrice = raffle.rafflePool.div(raffle.totalVolumeofTickets);
@@ -100,7 +123,10 @@ contract nftRafflerOriginx is IERC721Receiver {
 
     function cancelRaffle(uint256 _raffleID) public {
         Raffle storage raffle = raffles[_raffleID];
-        require(raffle.raffleCreator == msg.sender, "Only raffle creator can cancel raffle");
+        require(
+            raffle.raffleCreator == msg.sender,
+            "Only raffle creator can cancel raffle"
+        );
         require(raffle.raffleCancelled == false, "Raffle already cancelled");
 
         raffle.raffleCancelled = true;
@@ -108,8 +134,13 @@ contract nftRafflerOriginx is IERC721Receiver {
         IERC721 nftContract = IERC721(raffle.nftContractAddress);
         nftContract.safeTransferFrom(address(this), msg.sender, raffle.nftId);
     }
-    
-    function onERC721Received(address operator, address from, uint256 tokenId, bytes calldata data) external returns (bytes4) {
-    return this.onERC721Received.selector;
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external returns (bytes4) {
+        return this.onERC721Received.selector;
     }
-    }
+}

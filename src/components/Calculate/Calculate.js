0af8fcdd44e071c3fa4a91ac2664d123dc9183d7
manoranjith-shankar <21csbs023@skcet.ac.in>
2023-06-22 12:06:50 +0530

    update

--- src/components/Calculate/Calculate.js
+++ src/components/Calculate/Calculate.js
@@ -4,122 +4,29 @@ const Calculate = () => {
 
   const nftPrice = 0.2564
 
-  const rafflePool = ((nftPrice * 50) / 100) + nftPrice
-  console.log(rafflePool);
+  const handleClick = () => {
+  
+    const rafflePool = ((nftPrice * 50) / 100) + nftPrice
 
-  // return (
-  //   <section className={`author-area`}>
-  //     <div className="container">
-  //       <div className="row justify-content-center">
-  //         <div className="col-12 col-md-7">
-  //           {/* Intro */}
-  //           <div className="intro mt-5 mt-lg-0 mb-4 mb-lg-5">
-  //             <div className="intro-content">
-  //               <h3 className="mt-3 mb-0">Calculate Raffle</h3>
-  //             </div>
-  //           </div>
-  //           {/* Create Raffle form */}
-  //           <form onSubmit={handlePrice} className="item-form card no-hover">
-  //             <div className="row">
-  //               <div className="col-12">
-  //                 <div className="form-group mt-3">
-  //                   <input
-  //                     type="text"
-  //                     className="form-control"
-  //                     name="name"
-  //                     placeholder="Raffle Name"
-  //                     required="required"
-  //                     value={raffleName}
-  //                     onChange={(e) => setRaffleName(e.target.value)}
-  //                   />
-  //                 </div>
-  //               </div>
-  //               <div className="col-12 col-md-6">
-  //                 <div className="form-group">
-  //                   <input
-  //                     type="number"
-  //                     className="form-control"
-  //                     name="price"
-  //                     placeholder="NFT Price"
-  //                     required="required"
-  //                     value={nftPrice}
-  //                     onChange={(e) => setNftPrice(e.target.value)}
-  //                   />
-  //                 </div>
-  //               </div>
-  //               <div className="col-12 col-md-6">
-  //                 <div className="form-group">
-  //                   <input
-  //                     type="number"
-  //                     className="form-control"
-  //                     name="royality"
-  //                     placeholder="Total Ticket Supply"
-  //                     required="required"
-  //                     value={totalVolumeofTickets}
-  //                     onChange={(e) => setTotalVolumeofTickets(e.target.value)}
-  //                   />
-  //                 </div>
-  //               </div>
-  //               <div className="col-12 col-md-6">
-  //                 <div className="form-group">
-  //                   <input
-  //                     type="text"
-  //                     className="form-control"
-  //                     placeholder="Token ID (NFT)"
-  //                     required="required"
-  //                     value={nftId}
-  //                     onChange={(e) => setNftId(e.target.value)}
-  //                   />
-  //                 </div>
-  //               </div>
-  //               <div className="col-12">
-  //                 <div className="form-group">
-  //                   <input
-  //                     type="text"
-  //                     className="form-control"
-  //                     name="price"
-  //                     placeholder="NFT Contract Address"
-  //                     required="required"
-  //                     value={nftContractAddress}
-  //                     onChange={(e) => setNftContractAddress(e.target.value)}
-  //                   />
-  //                 </div>
-  //               </div>
-  //               <div className="col-12">
-  //                 <div className="form-group">
-  //                   <input
-  //                     type="text"
-  //                     className="form-control"
-  //                     name="price"
-  //                     placeholder="NFT Source Link"
-  //                     required="required"
-  //                     value={nftSourceLink}
-  //                     onChange={(e) => setNftSourceLink(e.target.value)}
-  //                   />
-  //                 </div>
-  //               </div>
-  //               {/* Add Fractional NFT option */}
-  //               <div className="col-12">
-  //                 <button className={`btn w-100 mt-3 mt-sm-4`} type="submit">
-  //                   Create Raffle
-  //                 </button>
-  //                 <Toaster
-  //                   position="bottom-right"
-  //                   reverseOrder={true}
-  //                   toastOptions={{
-  //                     className: "",
-  //                     duration: 5000,
-  //                     style: { background: "#363636", color: "#fff" },
-  //                   }}
-  //                 />
-  //               </div>
-  //             </div>
-  //           </form>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </section>
-  // );
+    const raffleCreatorPrize = nftPrice + ((nftPrice * 5) / 100)
+    const developementTeamPrize = (nftPrice * 5) / 100
+    const minusPrize = raffleCreatorPrize + developementTeamPrize
+    const charityPrize = rafflePool - minusPrize
+
+    console.log(rafflePool)
+    console.log(raffleCreatorPrize)
+    console.log(developementTeamPrize)
+    console.log(minusPrize)
+    console.log(charityPrize)
+  }
+
+  return (
+    <section className={`author-area`}>
+      <div>
+        <button onClick={handleClick}>Txt</button>
+      </div>
+    </section>
+  );
 };
 
 export default Calculate;

0150de5aaf2d2b1afe61e6ddebc68d04b1ea14a5
manoranjith-shankar <21csbs023@skcet.ac.in>
2023-12-06 00:51:12 +0530

    update

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ This is the starting point of development of ORIGINX-DAPP-VERSION-2.
 ## *176+* - originX v2 using CNA (some backend logic is implemented with v1 reference, hence this v2 is not an entirely new dApp, but a improved version of v1 with added chainlink services)
 
 ## This version is created as a Project to be presented in **CONSTELLATION: A CHAINLINK HACKATHON**
+
+# List of to-dos
+> Here goes some future commit infos
+    - [x] Theme Finalizing Tailwind config, truffle config
+    - [] Hero, Features, Benefits intital Design Implementation
+    - [] Some Backend Logic Implementation
+    - [] .... (will be updated soon)
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,45 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    screens: {
+      sm: "575px",
+      md: "768px",
+      lg: "1025px",
+      xl: "1202px",
+    },
+    fontFamily: {
+      display: ['"CalSans-SemiBold"', "sans-serif"],
+      body: ['"DM Sans"', "sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      accent: "#8358FF",
+      "accent-dark": "#7444FF",
+      "accent-light": "#9E7CFF",
+      "accent-lighter": "#B9A0FF",
+      "light-base": "#F5F8FA",
+      green: "#10b981",
+      orange: "#FEB240",
+      red: "#EF4444",
+      blue: "#428AF8",
+      jacarta: {
+        base: "#5A5D79",
+        50: "#F4F4F6",
+        100: "#E7E8EC",
+        200: "#C4C5CF",
+        300: "#A1A2B3",
+        400: "#7D7F96",
+        500: "#5A5D79",
+        600: "#363A5D",
+        700: "#131740",
+        800: "#101436",
+        900: "#0D102D",
       },
     },
   },
